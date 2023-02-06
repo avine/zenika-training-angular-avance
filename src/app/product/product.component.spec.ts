a8391388e3649b5343552a5e2c9bbb4f669936b8
Stéphane Francel <stephane.francel@gmail.com>
2023-02-06 13:49:21 +0000

    Add unit tests

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
 import { Product } from '../model/product';
+import { By } from '@angular/platform-browser';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
@@ -23,5 +24,26 @@ describe('ProductComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit product on click', () => {
+    const spy = spyOn(component.addToBasket, 'emit');
+
+    const button = fixture.debugElement.query(By.css('.btn')).nativeElement as HTMLButtonElement;
+    button.click();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add class when product stock is 1', () => {
+    const div = fixture.debugElement.query(By.css('.product-item')).nativeElement as HTMLDivElement;
+
+    component.data = new Product('title', 'description', 'photo', 42, 2);
+    fixture.detectChanges();
+    expect(div.classList).not.toContain('last');
+
+    component.data = new Product('title', 'description', 'photo', 42, 1);
+    fixture.detectChanges();
+    expect(div.classList).toContain('last');
   });
 });
